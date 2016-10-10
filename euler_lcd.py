7ee30fdeef4a8c5e369fcf2d9c6159046b20b62e
mattpurcell <matthewpurcell@Matthews-MacBook-Air-3.local>
2016-10-10 14:05:54 -0400

    adding euler_lcd.py

---
+++ euler_lcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+def smallestDiv(n):
+    divisors = [x for x in range(1,(n+1))]    # get divisors
+    for i in xrange(2520,sys.maxint,n):
+        if(all(i%x == 0 for x in divisors)):
+            return i
+
+print (smallestDiv(20))
